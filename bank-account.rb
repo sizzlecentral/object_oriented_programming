0701c06cbdb7d7a7230f12c2f179c987c83c5d69
Don Cooper <don_cooper@hotmail.com>
2017-05-19 13:19:31 -0400

    Deposit working

--- bank-account.rb
+++ bank-account.rb
@@ -1,2 +1,33 @@
 # Excercise 1 / Bank Account
 #---------------------------
+
+class BankAccount
+
+  def initialize(balance, interest_rate)
+    @balance = balance
+    @interest_rate = interest_rate
+  end
+
+  def balance
+    @balance
+  end
+
+  def interest_rate
+    @interest_rate
+  end
+
+  def deposit(amount)
+    @amount = amount
+    @balance += @amount
+  end
+
+
+
+
+end
+
+# --- End of BankAccount class --- #
+
+chequing = BankAccount.new(0, 5)
+chequing.deposit(20)
+puts chequing.balance
