104865d801c89000c145616f7a06dd1db1ceffec
Don Cooper <don_cooper@hotmail.com>
2017-05-19 15:01:53 -0400

    Done level up

--- player.rb
+++ player.rb
@@ -3,13 +3,39 @@
 
 class Player
 
-  def initialize(gold_coins, health_points, lives)
-    @gold_coins = gold_coins
-    @health_points = health_points
-    @lives = lives
+  def initialize
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
   end
+
+  def gold_coins
+    @gold_coins
+  end
+
+  def health_points
+    @health_points
+  end
+
+  def lives
+    @lives
+  end
+
+  def level_up
+    @level_up = @lives += 1
+  end
+
+  def collect_treasure
+    @collect_treasure = @gold_coins += 1
+  end
+
 
 
 end
 
 # ----- End of Player class ----- #
+
+player1 = Player.new
+player1.level_up
+puts player1.lives
+puts player1.collect_treasure
