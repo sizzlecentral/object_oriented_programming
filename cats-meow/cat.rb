bf842dd6ab9f0dfd6e06d16ecc0141bd528d1afe
Don Cooper <don_cooper@hotmail.com>
2017-05-19 14:23:51 -0400

    Done 'eats at' excercise

--- cats-meow/cat.rb
+++ cats-meow/cat.rb
@@ -9,5 +9,36 @@ class Cat
     @meal_time = meal_time
   end
 
+  def name
+    @name
+  end
+
+  def preferred_food
+    @preferred_food
+  end
+
+  def meal_time
+    @meal_time
+  end
+
+  def eats_at
+    @eats_at
+    if @meal_time == 0
+      return "12am"
+    elsif @meal_time == 12
+      return "12pm"
+    elsif @meal_time <= 11
+      return "#{@meal_time}am"
+    else
+      return "#{@meal_time - 12}pm"
+    end
+  end
 
 end
+
+# ----- End of Cat class ----- #
+
+marvin = Cat.new("Marvin", "Friskies", 7)
+smoky = Cat.new("Smoky", "Fancy Feast", 12)
+
+puts smoky.eats_at
