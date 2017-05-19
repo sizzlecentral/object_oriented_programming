a9fe008b037fe596d8eb8ab65a7ddfabae094f42
Don Cooper <don_cooper@hotmail.com>
2017-05-19 13:37:22 -0400

    Cat's Meow started, Cat class created

---
+++ cats-meow/cat.rb
@@ -0,0 +1,13 @@
+# Excercise 2 / The Cat's Meow
+#-----------------------------
+
+class Cat
+
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
+
+
+end
